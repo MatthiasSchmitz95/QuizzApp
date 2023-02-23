20ad44c685aa830dbee9aaa794f40a66a0747fb1
Matthias Schmitze <m6schmitz123@gmail.comm>
2023-02-23 13:02:36 +0100

    working

--- script.js
+++ script.js
@@ -25,14 +25,20 @@ let questions = [{
     "right_answer": 2
 
 }]
-let currentQuestion = questions[0];
+let currentQuestion = 0;
+let rightAnswers = 0;
+let wrongAnswers = 0;
+
 
 function init() {
-    document.getElementById('question-footer').innerHTML = questions.length;
+    document.getElementById('totalQuestions').innerHTML = questions.length;
+}
+
+function questionProgress() {
+    document.getElementById('CurrentQustion').innerHTML = currentQuestion + 1;
 }
 
 function defaultBorder() {
-
     document.getElementById('geographic').classList.remove('clicked-border');
     document.getElementById('history').classList.remove('clicked-border');
     document.getElementById('codeing').classList.remove('clicked-border');
@@ -47,59 +53,112 @@ function borderChange(id) {
 }
 
 function renderQuestionCard() {
-
     document.getElementById('container-content').innerHTML = '';
-    document.getElementById('container-content').innerHTML += `
+    templateCard();
+}
+
+
+function renderTask() {
+
+    if (currentQuestion >= questions.length) {
+        document.getElementById('question').innerHTML = '';
+        
+        document.getElementById('questionFooter').innerHTML = '';
+        document.getElementById('taskContainer').innerHTML = `
+    <div class="endgame">
+    <h2>Du hast alle Fragen beantwortet. Du hattest ${rightAnswers}Antworten richtig und ${wrongAnswers} falsch.</h2>
+    
+    <span>${rightAnswers}/${questions.length}</span>
+    </div>
+    
+    `;
+
+    } else {
+        document.getElementById('question').innerHTML = questions[currentQuestion]['question'];
+        document.getElementById('answer1').innerHTML = questions[currentQuestion]['answer_1'];
+        document.getElementById('answer2').innerHTML = questions[currentQuestion]['answer_2'];
+        document.getElementById('answer3').innerHTML = questions[currentQuestion]['answer_3'];
+        document.getElementById('answer4').innerHTML = questions[currentQuestion]['answer_4'];
+    }
+}
+
+
+function selectAnswer(answer) {
+    let lastChar = answer.at(-1);
+    let idOfRightAnswer = `answer${questions[currentQuestion]['right_answer']}`;
+    if (questions[currentQuestion]['right_answer'] == lastChar) {
+
+        document.getElementById(answer).classList.add('right-answer');
+        rightAnswers++;
+
+    }
+    else {
+        document.getElementById(answer).classList.add('wrong-answer');
+        document.getElementById(idOfRightAnswer).classList.add('right-answer');
+        wrongAnswers++;
+
+    }
+    document.getElementById('next').disabled = false;
+   
+
+}
+
+function resetAnswers() {
+    document.getElementById('answer1').classList.remove('right-answer', 'wrong-answer')
+    document.getElementById('answer2').classList.remove('right-answer', 'wrong-answer')
+    document.getElementById('answer3').classList.remove('right-answer', 'wrong-answer')
+    document.getElementById('answer4').classList.remove('right-answer', 'wrong-answer')
+
+
+}
+
+function nextQuestion() {
+    let endgame = questions.length - 1;
+    currentQuestion++;
+
+     if (currentQuestion == endgame) {
+         resetAnswers();
+         renderTask();
+         if (currentQuestion!=questions.length) {
+            questionProgress();
+        }
+        document.getElementById('next').disabled = true;
+         document.getElementById('next').innerHTML= 'Ergebnis';        
+     ;
+
+     } else {
+        document.getElementById('next').disabled = true;
+        resetAnswers();
+        renderTask();
+        if (currentQuestion!=questions.length) {
+            questionProgress();
+        }
+        
+    }
+}
+
+function templateCard() {
+    document.getElementById('container-content').innerHTML +=
+        `
     <div class="card">
     <img class="img-size" src="img/quizz.jpg">
     <h2 id="question">Question</h2>
-    <div class="link-container">
-        <a id="answer1" onclick="selectAnswer('answer1');return false;">Antwort_1</a>
-        <a id="answer2" onclick="selectAnswer('answer2');return false;">Antwort_1</a>
-        <a id="answer3" onclick="selectAnswer('answer3');return false;">Antwort_1</a>
-        <a id="answer4" onclick="selectAnswer('answer4');return false;">Antwort_1</a>
+    <div id="taskContainer" class="link-container">
+        <div class="answers" id="answer1" onclick=" selectAnswer('answer1');return false;">Antwort_1</div>
+        <div class="answers" id="answer2" onclick=" selectAnswer('answer2');return false;">Antwort_1</div>
+        <div class="answers" id="answer3" onclick=" selectAnswer('answer3');return false;">Antwort_1</div>
+        <div class="answers" id="answer4" onclick=" selectAnswer('answer4');return false;">Antwort_1</div>
     </div>
-    <div class="question-footer">
+    <div id="questionFooter" class="question-footer">
         <span>
-            <b>1</b> von <b id="question-footer">${questions.length}</b>
+            <b id="CurrentQustion">1</b> von <b id="totalQuestions">${questions.length}</b>
         </span>
-        <button>Next Question</button>
+        <button class="next" id="next" disabled onclick="nextQuestion()">Next Question</button>
 
 
     </div>
 
 </div>
     `;
-
-}
-
-
-
-function renderTask(question, answer1, answer2, answer3, answer4) {
-    document.getElementById(question).innerHTML = currentQuestion['question'];
-    document.getElementById(answer1).innerHTML = currentQuestion['answer_1'];
-    document.getElementById(answer2).innerHTML = currentQuestion['answer_2'];
-    document.getElementById(answer3).innerHTML = currentQuestion['answer_3'];
-    document.getElementById(answer4).innerHTML = currentQuestion['answer_4'];
-
-
-
-}
-
-function selectAnswer(answer) {
-    let lastChar = answer.at(-1);
-    let idOfRightAnswer = `answer${currentQuestion['right_answer']}`;
-    if (currentQuestion['right_answer'] == lastChar) {
-        console.log('richtige Antwort');
-         document.getElementById(answer).classList.add('right-answer');
-        // renderQuestionCard();
-    }
-    else {
-        document.getElementById(answer).classList.add('wrong-answer');
-        document.getElementById(idOfRightAnswer).classList.add('right-answer');
-        // renderQuestionCard();
-        console.log('falsche Antwort');
-    }
-
 
 }
